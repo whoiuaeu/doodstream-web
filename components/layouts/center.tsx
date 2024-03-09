e3405704e03572d6b826bef5fc2c20189b382b0f
whoiuaeu <155434872+whoiuaeu@users.noreply.github.com>
2024-03-10 03:24:07 +0700

    center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Center = ({ children }: { children: React.ReactNode }) => {
     return (  
-        <div className="flex flex-col h-[50vh] justify-center items-center">
+         <div className="flex flex-col h-[50vh] justify-center items-center">
             {children}
         </div>
     );
