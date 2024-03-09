ae6bffc4f0c60f2327e8c5c8d88e5ef81100090b
whoiuaeu <155434872+whoiuaeu@users.noreply.github.com>
2024-03-10 02:59:35 +0700

    Update center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Center = ({ children }: { children: React.ReactNode }) => {
-    return (
+    return (  
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fweb-banners-template-set-with-text-space_1017-30732.jpg&tbnid=yUynvSAnEmtdvM&vet=12ahUKEwik8bD5-ueEAxWx1zgGHfjDCwUQMygBegQIARBR..i&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fwebsite-banner&docid=u0TyUOIAXHD2ZM&w=626&h=514&q=banner&ved=2ahUKEwik8bD5-ueEAxWx1zgGHfjDCwUQMygBegQIARBR">
         <div className="flex flex-col h-[50vh] justify-center items-center">
             {children}
         </div>
